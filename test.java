43983b0353de687f82b773ea10747f5a340eada1
DAFZIE <daffa.akmal51@ui.ac.id>
2026-02-02 11:37:02 +0700

    feat: 1-Menampilkan Pesan Pembuka dan Penutup

---
+++ test.java
@@ -0,0 +1,15 @@
+public class test {
+    private static final String BANNER = "   ___           __             ____               __ \r\n" + //
+                "  / _ )__ ______/ /  ___ ____  / __ \\__ _____ ___ / /_\r\n" + //
+                " / _  / // / __/ _ \\/ _ `/ _ \\/ /_/ / // / -_|_-</ __/\r\n" + //
+                "/____/\\_,_/_/ /_//_/\\_,_/_//_/\\___\\_\\_,_/\\__/___/\\__/ ";
+    private static final String STUDENT_NAME = "Daffa Akmal";
+    private static final String STUDENT_ID = "2506584451";
+
+    public static void main(String[] args) {
+        // Sub-task 1 - Menampilkan Pesan Pembuka
+        System.out.println(BANNER);
+        System.out.println("Selamat datang di BurhanQuest!");
+        System.out.println("Dibuat oleh: " + STUDENT_NAME + " - " + STUDENT_ID);
+    }
+}
